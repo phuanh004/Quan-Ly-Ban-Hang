e07080db391f24511ca9deb067e4c20d08fb336b
phuanh004 <anhpham@Anhs-MacBook-Pro.local>
2016-08-03 22:41:34 +0700

    Update DB

--- connect_db.php
+++ connect_db.php
@@ -1,14 +1,39 @@
 <?php
-try
-{
-    $serverName = "tcp:phuanh004.database.windows.net,1433";
-    $connectionOptions = array("Database"=>"AdventureWorks",
-        "Uid"=>"phuanh004", "PWD"=>"Anhpham123");
-    $conn = sqlsrv_connect($serverName, $connectionOptions);
-    if($conn == false)
-        die(FormatErrors(sqlsrv_errors()));
+ReadData();
+
+function OpenConnection(){
+    try {
+        $serverName = "tcp:phuanh004.database.windows.net,1433";
+        $connectionOptions = array("Database"=>"AdventureWorks",
+            "Uid"=>"phuanh004", "PWD"=>"Anhpham123");
+        $conn = sqlsrv_connect($serverName, $connectionOptions);
+        if($conn == false)
+            die(FormatErrors(sqlsrv_errors()));
+    }
+    catch(Exception $e){
+        echo("Error!");
+    }
 }
-catch(Exception $e)
-{
-    echo("Error!");
+function ReadData(){
+    try {
+//        $conn = OpenConnection();
+        echo 'success';
+//        $tsql = "SELECT [CompanyName] FROM SalesLT.Customer";
+//        $getProducts = sqlsrv_query($conn, $tsql);
+//        if ($getProducts == FALSE)
+//            die(FormatErrors(sqlsrv_errors()));
+//        $productCount = 0;
+//        while($row = sqlsrv_fetch_array($getProducts, SQLSRV_FETCH_ASSOC))
+//        {
+//            echo($row['CompanyName']);
+//            echo("<br/>");
+//            $productCount++;
+//        }
+//        sqlsrv_free_stmt($getProducts);
+//        sqlsrv_close($conn);
+    }
+    catch(Exception $e)
+    {
+        echo("Error!");
+    }
 }
